2aef23d7408026e759e66a967637a9b3d0f35534
michael <michael@pop-os.localdomain>
2020-01-31 10:52:25 -0500

    Lab completed 1/31/2020

--- week-2/lab/group_work/Tickets.java
+++ week-2/lab/group_work/Tickets.java
@@ -1,8 +1,16 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.FileWriter;
+
+//Michael Haupt
+
 public class Tickets{
 
     public static void main(String[] args){
 
-        /* 
+        /*
             The following code steps you through the String processing
             and math neccessary to determine if a single ticket number is valid.
             Your job with this code is to:
@@ -25,18 +33,43 @@ public class Tickets{
             Check your work: There are 101 valid ticket numbers among the 1000 provided ticket numbers
                              These people make mistakes!!!
 
-            Submit only Java source code files. Also submit valid_tix.txt   
+            Submit only Java source code files. Also submit valid_tix.txt
 
         */
 
-        String  ticket  = "123454";                            
-        String  last = ticket.substring(ticket.length() - 1);                                                   
-        int     last_digit = Integer.valueOf(last);
-        String  reduced_ticket = ticket.substring(0, ticket.length() - 1);
-        int     ticket_number = Integer.valueOf(reduced_ticket);
-        int     remainder = ticket_number % 7; 
-        boolean validity = remainder == last_digit; 
-        String  format = "Original Ticket #: %s\nLast Digit: %d\nReduced Ticket #: %d\nRemainder: %d\nValidity: %b\n";
-        System.out.printf(format, ticket, last_digit, ticket_number, remainder, validity);  
+        int countValidTik = 0; //keep track of the number of valid tickets
+
+        try{
+            File ticketList = new File("tickets.txt"); //create File instance to reference txt file
+            Scanner ticketScanner = new Scanner(ticketList); //pass File object into the Scanner
+            FileWriter validTicket = new FileWriter("valid_tix.txt"); //creat FileWriter instance to reference txt file
+
+
+            while(ticketScanner.hasNext()){ //while loop will run while hasNext remains true
+                String line = ticketScanner.nextLine(); //get the next line and save it to a string
+                String  last = line.substring(line.length() - 1); //save the last digit of the ticket number in a string
+                int     last_digit = Integer.valueOf(last); //change last digit string to integer
+                String  reduced_ticket = line.substring(0, line.length() - 1); //create string of ticket without last number
+                int     ticket_number = Integer.valueOf(reduced_ticket); //convert reduced ticket to an integer
+                int     remainder = ticket_number % 7; //modulus division on the reduced ticket number
+                boolean validity = remainder == last_digit; //checks if remainder is identical to the last digit
+                if(validity == true){ //if
+                    countValidTik++;
+                    validTicket.write(line + "\n");
+                }
+            }
+
+            ticketScanner.close(); //close the Scanner
+            validTicket.close(); //close the file writer
+
+        } catch(FileNotFoundException fnf){ //catch read file error
+            System.out.println("Error: file not found");
+        }
+        catch(IOException ioE) { //catch write file error
+            System.out.println("Error: Cannot print to output");
+        }
+        
+        System.out.println("Valid Ticket Count: " + countValidTik); //print out the valid ticket count
+
     }
 }
